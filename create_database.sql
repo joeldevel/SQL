928f106d98b244cf18bc894a89371066549c6326
joeldevel <christianjoel.developer@gmail.com>
2022-11-19 02:18:32 -0300

    Data definition statements. How to create a database. How to create a table

---
+++ create_database.sql
@@ -0,0 +1,8 @@
+-- How to create a database
+-- CREATE DATABASE database_name
+
+-- mysql 8
+-- see: https://dev.mysql.com/doc/refman/8.0/en/create-database.html
+
+CREATE DATABASE music_store;
+CREATE SCHEMA music_store;
---
+++ create_table.sql
@@ -0,0 +1,12 @@
+-- How to create a table:
+-- CREATE TABLE table_name
+-- CREATE GLOBAL TEMPORARY TABLE table_name
+-- CREATE LOCAL TEMPORARY TABLE table_name
+
+-- column_name type
+
+-- mysql 8
+-- see: https://dev.mysql.com/doc/refman/8.0/en/create-table.html
+CREATE TABLE artists ( 
+	id INTEGER, 
+	name CHAR(60));
